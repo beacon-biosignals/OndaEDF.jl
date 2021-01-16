c1b01b0e03f3dc907cae6c66cbc360a8dbafebd8
Jarrett Revels <jarrettrevels@gmail.com>
2021-01-16 16:12:32 -0500

    remove A1/A2 <-> M1/M2 channel aliasing
    It turns out that these are not the same, though they are similarly used as references. 
    
    From @ararslan: "M1/2 are the mastoids and A1/2 are earlobes."

--- src/standards.jl
+++ src/standards.jl
@@ -66,7 +66,7 @@ const STANDARD_LABELS = Dict(# This EEG channel name list is a combined 10/20 an
                                         :af7, :af3, :afz, :af4, :af8,
                                         :f9, :f7, :f5, :f3, :f1, :fz, :f2, :f4, :f6, :f8, :f10,
                                         :ft9, :ft7, :fc5, :fc3, :fc1, :fcz, :fc2, :fc4, :fc6, :ft8, :ft10,
-                                        :a1 => [:m1], :t9, :t7, :t3, :c5, :c3, :c1, :cz, :c2, :c4, :c6, :t4, :t8, :t10, :a2 => [:m2],
+                                        :a1, :m1, :t9, :t7, :t3, :c5, :c3, :c1, :cz, :c2, :c4, :c6, :t4, :t8, :t10, :a2, :m2,
                                         :tp9, :tp7, :cp5, :cp3, :cp1, :cpz, :cp2, :cp4, :cp6, :tp8, :tp10,
                                         :t5, :p9, :p7, :p5, :p3, :p1, :pz, :p2, :p4, :p6, :p8, :p10, :t6,
                                         :po7, :po3, :poz, :po4, :po8,
