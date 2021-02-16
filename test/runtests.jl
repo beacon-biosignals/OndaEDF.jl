7c089708847d4fcf6d65960951a4ee6ee6a1f9c1
Jarrett Revels <jarrettrevels@gmail.com>
2021-02-16 15:51:06 -0500

    add L/R aliases for left/right in various standard signals (#9)
    * add L/R aliases for left/right in various standard signals
    
    * bump Project.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Test, Dates, Random, UUIDs, Statistics
 using OndaEDF, Onda, EDF
 
 @testset "EDF.Signal label handling" begin
-    signal_names = [:eeg, :test]
+    signal_names = [:eeg, :eog, :test]
     canonical_names = OndaEDF.STANDARD_LABELS[[:eeg]]
     @test OndaEDF.match_edf_label("EEG C3-(M1 +A2)/2 - rEf3", signal_names, :c3, canonical_names) == Symbol("c3-m1_plus_a2_over_2")
     @test OndaEDF.match_edf_label("EEG C3-(M1 +A2)/2 - rEf3", [:ecg], :c3, canonical_names) == nothing
@@ -10,6 +10,8 @@ using OndaEDF, Onda, EDF
     @test OndaEDF.match_edf_label(" TEsT   -Fpz  -REF-cpz", signal_names, :fpz, canonical_names) == Symbol("-fpz-ref-cpz")
     @test OndaEDF.match_edf_label(" TEsT   -Fpz  -REF-cpz", signal_names, :fp, canonical_names) == nothing
     @test OndaEDF.match_edf_label("  -Fpz  -REF-cpz", signal_names, :fpz, canonical_names) == nothing
+    @test OndaEDF.match_edf_label("EOG L", signal_names, :left, OndaEDF.STANDARD_LABELS[[:eog]]) == :left
+    @test OndaEDF.match_edf_label("EOG R", signal_names, :right, OndaEDF.STANDARD_LABELS[[:eog]]) == :right
     for (signal_names, channel_names) in OndaEDF.STANDARD_LABELS
         for channel_name in channel_names
             name = channel_name isa Pair ? first(channel_name) : channel_name
