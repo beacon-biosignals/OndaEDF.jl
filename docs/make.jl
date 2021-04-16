1848a3765f6a42d6304fb7114ec9c78eab606a59
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2021-04-16 21:37:17 +0000

    gimme that preview

--- docs/make.jl
+++ docs/make.jl
@@ -6,4 +6,5 @@ makedocs(modules=[OndaEDF],
          authors="Beacon Biosignals and other contributors",
          pages=["API Documentation" => "index.md"])
 
-deploydocs(repo="github.com/beacon-biosignals/OndaEDF.jl.git")
+deploydocs(repo="github.com/beacon-biosignals/OndaEDF.jl.git",
+           push_preview=true)
