4b17fccac23d822fd78e956b07debb7a14c34340
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2021-04-20 09:44:18 -0400

    Update src/import_edf.jl
    Co-authored-by: Jarrett Revels <jarrettrevels@gmail.com>

--- src/import_edf.jl
+++ src/import_edf.jl
@@ -325,8 +325,12 @@ end
     edf_to_onda_annotations(edf::EDF.File, uuid::UUID)
 
 Extract EDF+ annotations from an `EDF.File` for recording with ID `uuid` and
-return them as a vector of `Onda.Annotation`s.  If no annotations are found then
-an empty `Vector{Annotation}` is returned.
+return them as a vector of `Onda.Annotation`s.  Each returned annotation has 
+a  `value` field that contains the string value of the corresponding EDF+ 
+annotation. 
+
+If no EDF+ annotations are found in `edf`, then an empty `Vector{Annotation}` is 
+returned.
 """
 function edf_to_onda_annotations(edf::EDF.File, uuid::UUID)
     EDF.read!(edf)
