9935cf30fc938ed574e28c7d7d2adbd2b75cfe5b
Curtis Vogt <curtis.vogt@gmail.com>
2022-01-10 14:50:45 -0600

    Use EDF 0.7 (#29)
    * Use EDF 0.7
    
    * Set package version to 0.9.2
    
    * support 24-bit sample types (BDF) (#35)
    
    Co-authored-by: klaff <david.klaffenbach@beacon.bio>
    
    Co-authored-by: Jarrett Revels <jarrettrevels@gmail.com>
    Co-authored-by: klaff <david.klaffenbach@beacon.bio>

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,51 +5,53 @@ using FilePathsBase: AbstractPath, PosixPath
 function test_edf_signal(rng, label, transducer, physical_units,
                          physical_min, physical_max,
                          digital_min, digital_max,
-                         samples_per_record, n_records)
+                         samples_per_record, n_records, ::Type{T}=Int16) where {T}
     header = EDF.SignalHeader(label, transducer, physical_units,
                               physical_min, physical_max,
                               digital_min, digital_max,
-                              "", Int16(samples_per_record))
-    samples = rand(rng, Int16, n_records * samples_per_record)
+                              "", convert(T, samples_per_record))
+    samples = rand(rng, T, n_records * samples_per_record)
     return EDF.Signal(header, samples)
 end
 
-function make_test_data(rng, sample_rate, samples_per_record, n_records)
-    imin16, imax16 = Float32(typemin(Int16)), Float32(typemax(Int16))
+function make_test_data(rng, sample_rate, samples_per_record, n_records, ::Type{T}=Int16) where {T}
+    imin16, imax16 = Float32(typemin(T)), Float32(typemax(T))
     anns_1 = [[EDF.TimestampedAnnotationList(i, nothing, []),
                EDF.TimestampedAnnotationList(i, i + 1, ["", "$i a", "$i b"])] for i in 1:n_records]
     anns_2 = [[EDF.TimestampedAnnotationList(i, nothing, []),
                EDF.TimestampedAnnotationList(i, 0, ["", "$i c", "$i d"])] for i in 1:n_records]
-    edf_signals = Union{EDF.AnnotationsSignal,EDF.Signal}[
-        test_edf_signal(rng, "EEG F3-M2", "E", "uV",  -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "EEG F4-M1", "E", "uV",  -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "EEG C3-M2", "E", "uV",  -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "EEG O1-M2", "E", "uV",  -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "C4-M1", "E", "uV",  -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "O2-A1", "E", "uV",  -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "E1", "E", "uV",  -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "E2", "E", "uV",  -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "Fpz",   "E", "uV",  -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        EDF.AnnotationsSignal(samples_per_record, anns_1)
-        test_edf_signal(rng, "EMG LAT",   "E", "uV",  -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "EMG RAT",   "E", "uV",  -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "SNORE", "E", "uV",  -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "IPAP", "", "cmH2O", -74.0465f0, 74.19587f0,  imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "EPAP", "", "cmH2O", -73.5019f0, 74.01962f0,  imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "CFLOW", "",  "LPM", -309.153f0, 308.8513f0,  imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "PTAF", "",     "v", -125.009f0, 125.009f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "Leak", "",   "LPM", -147.951f0, 148.4674f0,  imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "CHEST", "E",  "uV", -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "ABD", "E",    "uV", -32768.0f0, 32767.0f0,   imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "Tidal", "",   "mL", -4928.18f0, 4906.871f0,  imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "SaO2", "",    "%",       0.0f0,    100.0f0,  imin16, imax16, samples_per_record, n_records)
-        EDF.AnnotationsSignal(samples_per_record, anns_2)
-        test_edf_signal(rng, "EKG EKGR- REF", "E",  "uV",   -9324.0f0, 2034.0f0,  imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "IC", "E",    "uV",    -32768.0f0, 32767.0f0, imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "HR", "",    "BpM",    -32768.0f0, 32768.0f0, imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "EcG EKGL", "E",  "uV",   -10932.0f0, 1123.0f0,  imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "- REF", "E",  "uV",   -10932.0f0, 1123.0f0,  imin16, imax16, samples_per_record, n_records)
-        test_edf_signal(rng, "REF1", "E",  "uV",   -10932.0f0, 1123.0f0,  imin16, imax16, samples_per_record, n_records)
+    _edf_signal = (label, transducer, unit, lo, hi) -> test_edf_signal(rng, label, transducer, unit, lo, hi, imin16,
+                                                                       imax16, samples_per_record, n_records, T)
+    edf_signals = Union{EDF.AnnotationsSignal,EDF.Signal{T}}[
+        _edf_signal("EEG F3-M2", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("EEG F4-M1", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("EEG C3-M2", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("EEG O1-M2", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("C4-M1", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("O2-A1", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("E1", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("E2", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("Fpz", "E", "uV", -32768.0f0, 32767.0f0),
+        EDF.AnnotationsSignal(samples_per_record, anns_1),
+        _edf_signal("EMG LAT", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("EMG RAT", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("SNORE", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("IPAP", "", "cmH2O", -74.0465f0, 74.19587f0),
+        _edf_signal("EPAP", "", "cmH2O", -73.5019f0, 74.01962f0),
+        _edf_signal("CFLOW", "", "LPM", -309.153f0, 308.8513f0),
+        _edf_signal("PTAF", "", "v", -125.009f0, 125.009f0),
+        _edf_signal("Leak", "", "LPM", -147.951f0, 148.4674f0),
+        _edf_signal("CHEST", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("ABD", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("Tidal", "", "mL", -4928.18f0, 4906.871f0),
+        _edf_signal("SaO2", "", "%", 0.0f0, 100.0f0),
+        EDF.AnnotationsSignal(samples_per_record, anns_2),
+        _edf_signal("EKG EKGR- REF", "E", "uV", -9324.0f0, 2034.0f0),
+        _edf_signal("IC", "E", "uV", -32768.0f0, 32767.0f0),
+        _edf_signal("HR", "", "BpM", -32768.0f0, 32768.0f0),
+        _edf_signal("EcG EKGL", "E", "uV", -10932.0f0, 1123.0f0),
+        _edf_signal("- REF", "E", "uV", -10932.0f0, 1123.0f0),
+        _edf_signal("REF1", "E", "uV", -10932.0f0, 1123.0f0),
     ]
     seconds_per_record = samples_per_record / sample_rate
     edf_header = EDF.FileHeader("0", "", "", DateTime("2014-10-27T22:24:28"), true, n_records, seconds_per_record)
