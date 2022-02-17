a6ce7233d0c1a872a67d97243715a1c6522a0805
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2022-02-17 17:33:55 +0000

    split out old tests w/ diagnostics

--- test/runtests.jl
+++ test/runtests.jl
@@ -71,7 +71,7 @@ function make_test_data(rng, sample_rate, samples_per_record, n_records, ::Type{
                      :ptaf => [17])
 end
 
-include("test_edf_to_samples_info.in")
+# include("test_edf_to_samples_info.in")
 
 @testset "OndaEDF" begin
     include("signal_labels.jl")
