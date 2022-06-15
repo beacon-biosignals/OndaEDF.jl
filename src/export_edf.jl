31a597629916edf8f333457186d760f6c30cb417
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2022-06-15 13:41:13 -0400

    Update docs for `onda_to_edf` to accurately reflect input (#47)
    * update docs to reflect actual input expectations for onda_to_edf
    
    * bump patch to update stable docs
    
    * grammar

--- src/export_edf.jl
+++ src/export_edf.jl
@@ -132,22 +132,24 @@ end
 #####
 
 """
-    onda_to_edf(signals, annotations=[]; kwargs...)
+    onda_to_edf(samples::AbstractVector{<:Samples}, annotations=[]; kwargs...)
 
-Return an `EDF.File` containing signal data converted from the Onda [`signals`
-table](https://beacon-biosignals.github.io/Onda.jl/stable/#*.onda.signals.arrow-1)
-and (optionally) annotations from an [`annotations`
+Return an `EDF.File` containing signal data converted from a collection of Onda
+[`Samples`](https://beacon-biosignals.github.io/Onda.jl/stable/#Samples-1) and
+(optionally) annotations from an [`annotations`
 table](https://beacon-biosignals.github.io/Onda.jl/stable/#*.onda.annotations.arrow-1).
 
-Following the Onda v0.5 format, both `signals` and `annotations` can be any
-Tables.jl-compatible table (DataFrame, Arrow.Table, NamedTuple of vectors, vector of
-NamedTuples) which follow the signal and annotation schemas (respectively).
+Following the Onda v0.5 format, `annotations` can be any Tables.jl-compatible
+table (DataFrame, Arrow.Table, NamedTuple of vectors, vector of NamedTuples)
+which follows the [annotation
+schema](https://beacon-biosignals.github.io/Onda.jl/stable/#*.onda.annotations.arrow-1).
 
-Each `EDF.Signal` in the returned `EDF.File` corresponds to a channel of an `Onda.Signal`.
+Each `EDF.Signal` in the returned `EDF.File` corresponds to a channel of an
+input `Onda.Samples`.
 
-The ordering of `EDF.Signal`s in the output will match the order of the rows of
-the signals table (and within each channel grouping, the order of the signal's
-channels).
+The ordering of `EDF.Signal`s in the output will match the order of the input
+collection of `Samples` (and within each channel grouping, the order of the
+samples' channels).
 """
 function onda_to_edf(samples::AbstractVector{<:Samples}, annotations=[]; kwargs...)
     edf_header = onda_samples_to_edf_header(samples; kwargs...)
