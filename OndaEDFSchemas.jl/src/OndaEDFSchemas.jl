47dcc94d4c14d6bcc53e817cf1df2130849735ef
Chris de Graaf <me@cdg.dev>
2022-12-06 14:28:01 -0400

    Upgrade OndaEDFSchemas to Legolas 0.5, Onda 0.15

--- OndaEDFSchemas.jl/src/OndaEDFSchemas.jl
+++ OndaEDFSchemas.jl/src/OndaEDFSchemas.jl
@@ -1,34 +1,87 @@
 module OndaEDFSchemas
 
-using Legolas: @row, lift
+using Legolas: Legolas, @schema, @version, lift
 using Onda: LPCM_SAMPLE_TYPE_UNION, onda_sample_type_from_julia_type, convert_number_to_lpcm_sample_type
 
-export Plan, FilePlan
+export PlanV1, PlanV2, FilePlanV1, FilePlanV2
 
-"""
-    Plan = @row("ondaedf.plan@1",
-                # EDF.SignalHeader fields
-                label::String
-                transducer_type::String
-                physical_dimension::String
-                physical_minimum::Float32
-                physical_maximum::Float32
-                digital_minimum::Float32
-                digital_maximum::Float32
-                prefilter::String
-                samples_per_record::Int16
-                # EDF.FileHeader field,
-                seconds_per_record::Float64
-                # Onda.SamplesInfo fields (channels -> channel), may be missing
-                kind::Union{Missing,AbstractString}
-                channel::Union{Missing,AbstractString}
-                sample_unit::Union{Missing,AbstractString}
-                sample_resolution_in_unit::Union{Missing,LPCM_SAMPLE_TYPE_UNION}
-                sample_offset_in_unit::Union{Missing,LPCM_SAMPLE_TYPE_UNION}
-                sample_type::Union{Missing,AbstractString}
-                sample_rate::Union{Missing,LPCM_SAMPLE_TYPE_UNION}
-                # errors, use `nothing` to indicate no error
-                error::Union{Nothing,String})
+@schema "ondaedf.plan" Plan
+
+@version PlanV1 begin
+    # EDF.SignalHeader fields
+    label::String
+    transducer_type::String
+    physical_dimension::String
+    physical_minimum::Float32
+    physical_maximum::Float32
+    digital_minimum::Float32
+    digital_maximum::Float32
+    prefilter::String
+    samples_per_record::Int16
+    # EDF.FileHeader field
+    seconds_per_record::Float64
+    # Onda.SamplesInfo fields (channels -> channel), may be missing
+    kind::Union{Missing,AbstractString} = lift(String, kind)
+    channel::Union{Missing,AbstractString} = lift(String, channel)
+    sample_unit::Union{Missing,AbstractString} = lift(String, sample_unit)
+    sample_resolution_in_unit::Union{Missing,LPCM_SAMPLE_TYPE_UNION} = lift(convert_number_to_lpcm_sample_type, sample_resolution_in_unit)
+    sample_offset_in_unit::Union{Missing,LPCM_SAMPLE_TYPE_UNION} = lift(convert_number_to_lpcm_sample_type, sample_offset_in_unit)
+    sample_type::Union{Missing,AbstractString} = lift(onda_sample_type_from_julia_type, sample_type)
+    sample_rate::Union{Missing,LPCM_SAMPLE_TYPE_UNION} = lift(convert_number_to_lpcm_sample_type, sample_rate)
+    # errors, use `nothing` to indicate no error
+    error::Union{Nothing,String} = coalesce(error, nothing)
+end
+
+@version PlanV2 begin
+    # EDF.SignalHeader fields
+    label::String
+    transducer_type::String
+    physical_dimension::String
+    physical_minimum::Float32
+    physical_maximum::Float32
+    digital_minimum::Float32
+    digital_maximum::Float32
+    prefilter::String
+    samples_per_record::Int16
+    # EDF.FileHeader field
+    seconds_per_record::Float64
+    # Onda.SamplesInfo fields (channels -> channel), may be missing
+    sensor_type::Union{Missing,AbstractString} = lift(String, sensor_type)
+    channel::Union{Missing,AbstractString} = lift(String, channel)
+    sample_unit::Union{Missing,AbstractString} = lift(String, sample_unit)
+    sample_resolution_in_unit::Union{Missing,LPCM_SAMPLE_TYPE_UNION} = lift(convert_number_to_lpcm_sample_type, sample_resolution_in_unit)
+    sample_offset_in_unit::Union{Missing,LPCM_SAMPLE_TYPE_UNION} = lift(convert_number_to_lpcm_sample_type, sample_offset_in_unit)
+    sample_type::Union{Missing,AbstractString} = lift(onda_sample_type_from_julia_type, sample_type)
+    sample_rate::Union{Missing,LPCM_SAMPLE_TYPE_UNION} = lift(convert_number_to_lpcm_sample_type, sample_rate)
+    # errors, use `nothing` to indicate no error
+    error::Union{Nothing,String} = coalesce(error, nothing)
+end
+
+const PLAN_DOC = """
+    @version PlanV\$N begin
+        # EDF.SignalHeader fields
+        label::String
+        transducer_type::String
+        physical_dimension::String
+        physical_minimum::Float32
+        physical_maximum::Float32
+        digital_minimum::Float32
+        digital_maximum::Float32
+        prefilter::String
+        samples_per_record::Int16
+        # EDF.FileHeader field
+        seconds_per_record::Float64
+        # Onda.SamplesInfo fields (channels -> channel), may be missing
+        kind::Union{Missing,AbstractString}
+        channel::Union{Missing,AbstractString}
+        sample_unit::Union{Missing,AbstractString}
+        sample_resolution_in_unit::Union{Missing,LPCM_SAMPLE_TYPE_UNION}
+        sample_offset_in_unit::Union{Missing,LPCM_SAMPLE_TYPE_UNION}
+        sample_type::Union{Missing,AbstractString}
+        sample_rate::Union{Missing,LPCM_SAMPLE_TYPE_UNION}
+        # errors, use `nothing` to indicate no error
+        error::Union{Nothing,String}
+    end
 
 A type-alias for a Legolas row describing a single EDF signal-to-Onda channel
 conversion.  The columns are the union of
@@ -36,50 +89,38 @@ conversion.  The columns are the union of
 - the `seconds_per_record` field from `EDF.FileHeader` (mandatory)
 - fields from `Onda.SamplesInfo` (optional, may be `missing` to indicate failed
   conversion)
-- `error`, which is `nothing` for a conversion that is or is expected to be 
+- `error`, which is `nothing` for a conversion that is or is expected to be
   successful, and a `String` describing the source of the error (with backtrace)
   in the case of a caught error.
 
-The [`FilePlan`](@ref) extension contains two columns that give additional
-context about the conversion that only apply at the level of an `EDF.File`: 
-- `edf_signal_index` gives the index of the source EDF signal
-- `onda_signal_index` gives the index of the output `Onda.Samples`.  Note some
-  indices may be skipped in the output, so this is only to indicate order and
-  grouping.
-
+Differences between versions are:
+- `PlanV1` has a `kind` field, whereas in `PlanV2` this has been renamed `sensor_type`.
 """
-const Plan = @row("ondaedf.plan@1",
-                  # EDF.SignalHeader fields
-                  label::String,
-                  transducer_type::String,
-                  physical_dimension::String,
-                  physical_minimum::Float32,
-                  physical_maximum::Float32,
-                  digital_minimum::Float32,
-                  digital_maximum::Float32,
-                  prefilter::String,
-                  samples_per_record::Int16,
-                  # EDF.FileHeader field,
-                  seconds_per_record::Float64,
-                  # Onda.SamplesInfo fields (channels -> channel), may be missing
-                  kind::Union{Missing,AbstractString} = lift(String, kind),
-                  channel::Union{Missing,AbstractString} = lift(String, channel),
-                  sample_unit::Union{Missing,AbstractString} = lift(String, sample_unit),
-                  sample_resolution_in_unit::Union{Missing,LPCM_SAMPLE_TYPE_UNION} = lift(convert_number_to_lpcm_sample_type, sample_resolution_in_unit),
-                  sample_offset_in_unit::Union{Missing,LPCM_SAMPLE_TYPE_UNION} = lift(convert_number_to_lpcm_sample_type, sample_offset_in_unit),
-                  sample_type::Union{Missing,AbstractString} = lift(onda_sample_type_from_julia_type, sample_type),
-                  sample_rate::Union{Missing,LPCM_SAMPLE_TYPE_UNION} = lift(convert_number_to_lpcm_sample_type, sample_rate),
-                  # errors, use `nothing` to indicate no error
-                  error::Union{Nothing,String} = coalesce(error, nothing))
 
-"""
-    const FilePlan = @row("ondaedf.file-plan@1" > "ondaedf.plan@1",
-                          edf_signal_index::Int,
-                          onda_signal_index::Int)
+@doc PLAN_DOC PlanV1
+@doc PLAN_DOC PlanV2
+
+@schema "ondaedf.file-plan" FilePlan
+
+@version FilePlanV1 > PlanV1 begin
+    edf_signal_index::Int
+    onda_signal_index::Int
+end
+
+@version FilePlanV2 > PlanV2 begin
+    edf_signal_index::Int
+    onda_signal_index::Int
+end
+
+const FILE_PLAN_DOC = """
+    @version FilePlanV\$N > PlanV\$N begin
+        edf_signal_index::Int
+        onda_signal_index::Int
+    end
 
 Type alias for a Legolas row for one EDF signal-to-Onda channel conversion,
-which includes the columns of a [`Plan`](@ref) and additional file-level context:
-- `edf_signal_index` gives the index of the `signals` in the source `EDF.File` 
+which includes the columns of a [`PlanV1`](@ref) or [`PlanV2`](@ref) and additional file-level context:
+- `edf_signal_index` gives the index of the `signals` in the source `EDF.File`
   corresponding to this row
 - `onda_signal_index` gives the index of the output `Onda.Samples`.
 
@@ -87,8 +128,8 @@ Note that while the EDF index does correspond to the actual index in
 `edf.signals`, some Onda indices may be skipped in the output, so
 `onda_signal_index` is only to indicate order and grouping.
 """
-const FilePlan = @row("ondaedf.file-plan@1" > "ondaedf.plan@1",
-                      edf_signal_index::Int,
-                      onda_signal_index::Int)
+
+@doc FILE_PLAN_DOC FilePlanV1
+@doc FILE_PLAN_DOC FilePlanV2
 
 end # module
--- OndaEDFSchemas.jl/test/runtests.jl
+++ OndaEDFSchemas.jl/test/runtests.jl
@@ -7,61 +7,73 @@ using StableRNGs
 using Tables
 using Test
 
-function mock_plan(n; rng=GLOBAL_RNG)
-    tbl = [mock_plan(; rng) for _ in 1:n]
+function mock_plan(n; v, rng=GLOBAL_RNG)
+    tbl = [mock_plan(; v, rng) for _ in 1:n]
     return tbl
 end
 
-function mock_plan(; rng=GLOBAL_RNG)
+function mock_plan(; v, rng=GLOBAL_RNG)
     ingested = rand(rng, Bool)
+    specific_kwargs = if v == 1
+        (; kind=ingested ? "eeg" : missing)
+    elseif v == 2
+        (; sensor_type=ingested ? "eeg" : missing)
+    else
+        error("Invalid version")
+    end
     errored = !ingested && rand(rng, Bool)
-    return Plan(; label="EEG CZ-M1",
-                transducer_type="Ag-Cl electrode",
-                physical_dimension="uV",
-                physical_minimum=0.0,
-                physical_maximum=2.0,
-                digital_minimum=-1f4,
-                digital_maximum=1f4,
-                prefilter="HP 0.1Hz; LP 80Hz; N 60Hz",
-                samples_per_record=128,
-                seconds_per_record=1.0,
-                kind=ingested ? "eeg" : missing,
-                channel=ingested ? "cz-m1" : missing,
-                sample_unit=ingested ? "microvolt" : missing,
-                sample_resolution_in_unit=ingested ? 1f-4 : missing,
-                sample_offset_in_unit=ingested ? 1.0 : missing,
-                sample_type=ingested ? "float32" : missing,
-                sample_rate=ingested ? 1/128 : missing,
-                # errors, use `nothing` to indicate no error
-                error=errored ? "Error blah blah" : nothing)
+    PlanVersion = v == 1 ? PlanV1 : PlanV2
+    return PlanVersion(; label="EEG CZ-M1",
+                       transducer_type="Ag-Cl electrode",
+                       physical_dimension="uV",
+                       physical_minimum=0.0,
+                       physical_maximum=2.0,
+                       digital_minimum=-1f4,
+                       digital_maximum=1f4,
+                       prefilter="HP 0.1Hz; LP 80Hz; N 60Hz",
+                       samples_per_record=128,
+                       seconds_per_record=1.0,
+                       channel=ingested ? "cz-m1" : missing,
+                       sample_unit=ingested ? "microvolt" : missing,
+                       sample_resolution_in_unit=ingested ? 1f-4 : missing,
+                       sample_offset_in_unit=ingested ? 1.0 : missing,
+                       sample_type=ingested ? "float32" : missing,
+                       sample_rate=ingested ? 1/128 : missing,
+                       error=errored ? "Error blah blah" : nothing,
+                       specific_kwargs...)
 end
 
-function mock_file_plan(n; rng=GLOBAL_RNG)
-    tbl = [mock_file_plan(; rng) for _ in 1:n]
+function mock_file_plan(n; v, rng=GLOBAL_RNG)
+    tbl = [mock_file_plan(; v, rng) for _ in 1:n]
     return tbl
 end
 
-function mock_file_plan(; rng=GLOBAL_RNG)
-    plan = mock_plan(; rng)
-    return FilePlan(Tables.rowmerge(plan;
-                                    edf_signal_index=rand(rng, Int),
-                                    onda_signal_index=rand(rng, Int)))
+function mock_file_plan(; v, rng=GLOBAL_RNG)
+    plan = mock_plan(; v, rng)
+    PlanVersion = v == 1 ? FilePlanV1 : FilePlanV2
+    return PlanVersion(Tables.rowmerge(plan;
+                                       edf_signal_index=rand(rng, Int),
+                                       onda_signal_index=rand(rng, Int)))
 end
 
-@testset "ondaedf.plan@1" begin
-    rng = StableRNG(10)
-    plans = mock_plan(30; rng)
-    @test nothing === Legolas.validate(plans, Legolas.Schema("ondaedf.plan@1"))
-    tbl = Arrow.Table(Arrow.tobuffer(plans))
-    @test isequal(Tables.columntable(tbl), Tables.columntable(plans))
-end
+for v in [1, 2]
+    @testset "ondaedf.plan@$v" begin
+        rng = StableRNG(10)
+        plans = mock_plan(30; v, rng)
+        schema = Tables.schema(plans)
+        @test nothing === Legolas.validate(schema, Legolas.SchemaVersion("ondaedf.plan", v))
+        tbl = Arrow.Table(Arrow.tobuffer(plans; maxdepth=9))
+        @test isequal(Tables.columntable(tbl), Tables.columntable(plans))
+    end
 
-@testset "ondaedf.file-plan@1" begin
-    rng = StableRNG(11)
-    plans = mock_file_plan(50; rng)
-    @test nothing === Legolas.validate(plans, Legolas.Schema("ondaedf.file-plan@1"))
-    tbl = Arrow.Table(Arrow.tobuffer(plans))
-    @test isequal(Tables.columntable(tbl), Tables.columntable(plans))
+    @testset "ondaedf.file-plan@$v" begin
+        rng = StableRNG(11)
+        file_plans = mock_file_plan(50; v, rng)
+        schema = Tables.schema(file_plans)
+        @test nothing === Legolas.validate(schema, Legolas.SchemaVersion("ondaedf.file-plan", v))
+        tbl = Arrow.Table(Arrow.tobuffer(file_plans; maxdepth=9))
+        @test isequal(Tables.columntable(tbl), Tables.columntable(file_plans))
+    end
 end
 
 end
